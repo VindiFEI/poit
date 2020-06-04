89e5577e25d2818768461884a2f4d77db3d06672
VindiFEI <adam.bodorik@gmail.com>
2020-06-04 19:17:20 +0200

    Add files via upload

--- index.py
+++ index.py
@@ -1,175 +1,135 @@
-#!/usr/bin/python3
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import cgitb
+import MySQLdb
 import cgi
-cgitb.enable()
-print("Content-Type: text/html; charset=utf-8\n\n")
-import threading
-import socket
-import pymysql
-import pymysql.cursors
-import json
-from flask import Flask, request
+import unicodedata
+import codecs
+import sys
+def force_to_unicode(text):
+    "If text is unicode, it is returned as is. If it's str, convert it to Unicode using UTF-8 encoding"
+    return text if isinstance(text, unicode) else text.decode('utf-8')
+def index(req):
+    start = """\
+    <!DOCTYPE html>
+    <html>
+    <title>zad8</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <body>
 
-worker = None
-
-def socket_worker():
-    # pripojenie do databazy
-    connection = pymysql.connect(host='localhost', user='adamko', password='FCST1923', db='zadanie', charset='utf8mb4', cursorclass=pymysql.cursors.DictCursor)
-    
-    # otvorenie suboru  
+    <h1>Zadanie 8</h1>
+    <div class="container">
+	<h1>Naša olympijská história</h1>
+	<table class="data-table">
+    <thead>
+    <tr>
+    <th>Meno</th>
+    <th>Priezvisko</th>
+    <th>Rok</th>
+    <th>Miesto</th>
+    <th>Typ OH</th>
+    <th>Disciplína</th>
+    </tr>
+    </thead>
+    <tbody>
+    """
+    db = MySQLdb.connect(host="localhost",
+                         user="xpruzinsky",
+                         passwd="tyrellsux",
+                         db="prvadb")
+    db.set_character_set('utf8')
+    cur = db.cursor()
+    body = ""
     try:
-        f = open("/var/www/html/cgi-enabled/file/record.txt", "w")
-    except IOError:
-        print("chyba pri otvarani suboru")
-
- #       with connection.cursor() as cursor:
-#            # Read a single record
-#            sql = "SELECT `id`, `password` FROM `users` WHERE `email`=%s"
-#            cursor.execute(sql, ('webmaster@python.org',))
- #           result = cursor.fetchone()
-#            print(result)
+        cur.execute("SELECT * FROM osoby JOIN umiestnenia ON osoby.id_person=umiestnenia.id_person JOIN oh ON umiestnenia.id_oh=oh.id_oh")
+        results = cur.fetchall()
+       	for row in results:
+        	body = body + "\n<tr><td>"+str(row[1])+"</td><td>"+str(row[2])+"</td><td>"+str(row[16])+"</td><td>"+str(row[18])+", "+str(row[19])+"</td><td>"+str(row[15])+"</td><td>"+str(row[13])+"</td></tr>"
+    except Exception as err:
+        body = err
+    db.close
+    end = """\
+    </tbody>
+    </table>
+    <button type="button" onclick="location.href = './addperson';">Pridanie osoby</button>
+    </div>
+    </body>
+    </html>
+    """
+    whole = start + body + end
+    return whole
+def addperson(req):
+    start = """\
+    <!DOCTYPE html>
+    <html>
+    <title>zad8</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <body>
+    <h1>Zadanie 8</h1>
+    <div class="container">
+	<h1>Pridanie záznamu</h1>
+	<form method="get" action="./addpersondone">
+	<p>Meno: <input type="text" name="firstname">
+	Priezvisko: <input type="text" name="surname">
+	Rok: <input type="text" name="year">
+	Typ OH: <input type="text" name="typeOH">
+	Disciplína: <input type="text" name="discipline">
+	<input type="submit" value="submit"</p>
+	</form>
+    </div>
+    </body>
+    </html>
+    """
+    return start
+def addpersondone(req):
+    firstname = req.form.getfirst('firstname', '')
+    surname = req.form.getfirst('surname', '')
+    year = req.form.getfirst('year', '')
+    typeOH = req.form.getfirst('typeOH', '')
+    discipline = req.form.getfirst('discipline', '')
     
-    
-    #while True:
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s.bind(('0.0.0.0', 50000))
-    s.settimeout(0.5)
-    global data, addr
-    
-    while True:              
-        try:
-            data, addr = s.recvfrom(1024)
-            node = json.loads(data.decode("utf-8"))
-           # print(node)
-            
-            #zapis do suboru
-      #      f.write(str(Node))
-            f.write("ahoj")
-            
-            # vlhkost, vytiahnem z json stringu
-            humidity = float(node['humidity'])
-            
-             # teplota(C)
-            temperatureC = float(node["temperature(C)"])
-            
-             # teplota(F)
-            temperatureF = float(node["temperature(F)"])
-
-            # ppm
-            ppm = float(node["ppm"])
-            
-            with connection.cursor() as cursor:
-                # zapis do db
-                sql = "INSERT INTO data (temperatureC, temperatureF, humidity, ppm) VALUES ('%s', '%s', '%s', '%s')"
-                cursor.execute(sql, (temperatureC, temperatureF, humidity, ppm))
-                connection.commit()
-                
-                # dopplnit button, ktory vysle signal, aby sme vyskocili z cyklu
-                
-            
-            #chcelo by to spravit ako samostatnu funkciu, ale nejde. To by connection variable musela byt globalna
-            #dat if else na Celius alebo fahrenheit, podla toho sa spravi select
-            
-            cur = connection.cursor()
-            print("KABEL")
-            # Read a single record
-            sql_select = "SELECT * FROM data WHERE id = ( SELECT MAX(id) FROM data )"
-            cur.execute(sql_select)
-            result = cur.fetchone()
-            print(result)    
-
-            open_button()
-            
-                
-        except :
-            pass
-    
-    # spytat sa, preco mi nejde zatvorit suboru a tym padom aj ulozit udaje
-    f.close()
-    print(f.closed)
-    print("zatvaram subor")
-    # ukoncenie zapisu do db
-    connection.close()
-    
-def start_worker():
-    global worker
-    worker = threading.Thread(target=socket_worker)
-    worker.daemon = True
-    worker.start()
-    
-def open_button():
-    
-    #pridat vyber medzi Celsius a Fahrenheit
-
-# sem dopisat strukturu web stranky a pushnut to do browseru
-print("henlo\n")
-print("before worker\n")
-
-
-# pridat button na premazanie dat, to by bola fajn vec navyse
-
-#pridat button na vypisanie najvyssej nameranej teploty alebo vlhkosti
-
-# start worker by mal byt pusteny cez button
-start_worker()
-
-
-html = '''\
-<!DOCTYPE html>
-<html>
-    <title>moje kuzelne zadanie</title>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-<body>
-    <h1>Zadanie</h1>
-    <p>Hello There!</p>
-    <p>General Kenobi!</p>
-    <button type="button">Celsius</button>
-    <button type="button">Fahrenheit</button> 
-    <p>$node</p>
-</body>
-</html>
-'''
-print(html)
-
-
-#while True:
-
-#    client, addr = s.accept()
-    
-#    print('Connected with ' + addr[0] + ':' + str(addr[1]))
-
- #   while True:
-      #  content = client.recv(1024)
-
-     #   if len(content) == 0:
-      #      break
-
-    #    else:
-        #    print(content.decode("utf-8"))
-         #   node = json.loads(content.decode("utf-8"))
-         #   print("<p></p>")
-
-            # vlhkost, vytiahnem z json stringu
-         #   humidity = float(node["humidity"])
-
-            # teplota
-          #  temperature = float(node["temperature"])
-
-            # ppm
-          #  ppm = float(node["ppm"])
-
-            # f = open("/file/zapis", "a")
-            # f.write(node)
-            # f.close()
-    
-  #  client.close()
-
-
-
-
-#cele = Template(html)
-#print(cele)
-
-
+    firstname = cgi.escape(firstname)
+    surname = cgi.escape(surname)
+    year = cgi.escape(year)
+    typeOH = cgi.escape(typeOH)
+    discipline = cgi.escape(discipline)
+    s = """\
+    <!DOCTYPE html>
+    <html>
+    <title>zad8</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <body>
+    <h1>Zadanie 8</h1>
+    <div class="container">
+	<h1>Výsledok</h1>
+	"""
+    db = MySQLdb.connect(host="localhost",
+                         user="xpruzinsky",
+                         passwd="tyrellsux",
+                         db="prvadb")
+    db.set_character_set('utf8')
+    cur = db.cursor()
+    try:
+        cur.execute("""INSERT INTO osoby(firstname, surname) VALUES (%s, %s)""",(firstname.decode('utf-8'), surname.decode('utf-8')))
+        db.commit()
+        cur.execute("SELECT LAST_INSERT_ID()")
+        db.commit()
+        id_person = cur.fetchone()
+        cur.execute("""SELECT id_OH FROM oh WHERE year=%s AND type=%s """, (year, typeOH))
+        db.commit()
+        id_OH = cur.fetchone()
+        cur.execute("""INSERT INTO umiestnenia(id_person, id_OH, place, discipline) VALUES (%s, %s, %s, %s)""",(id_person, id_OH , "1", discipline.decode('utf-8')))
+        db.commit()
+    except Exception as err:
+        return err
+    db.close
+    e = """\
+    <p> Záznam bol uložený </p>
+    </div>
+    </body>
+    </html>
+    """
+    whole = s + e
+    return whole
